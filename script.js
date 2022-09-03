db97773cdc0f25ad02dadc12c71802e224284823
bobkovIBAS <va.bobkov@yandex.ru>
2022-09-04 01:18:56 +0400

    all done

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-var typeOfOperation;
-function Calculate()
-{
-    var result;
-    var TheFirstNumber = Number(document.getElementById("TheFirstElement").value);
-    var TheSecondNumber = Number(document.getElementById("TheSecondElement").value);
+function Calculate() {
+  let result;
+  let TheFirstNumber = Number(document.getElementById("TheFirstElement").value);
+  let TheSecondNumber = Number(document.getElementById("TheSecondElement").value);
+  let typeOfOperation = document.getElementById("typeOfOperation").value;
 
+  if (TheFirstNumber === "" || TheSecondNumber === "") {
+    alert("Имеются незаполненные поля");
+    breal
+  } else {
     switch (typeOfOperation) {
-        case '+':
-            result = TheFirstNumber + TheSecondNumber;
-            break;
-        case '-': 
-            result = TheFirstNumber - TheSecondNumber;
-            break;
-        case '*':
-            result = TheFirstNumber * TheSecondNumber;
-            break;
-        case '/':   
-            result = TheFirstNumber / TheSecondNumber;
-            break;
+      case "+":
+        result = TheFirstNumber + TheSecondNumber;
+        break;
+      case "-":
+        result = TheFirstNumber - TheSecondNumber;
+        break;
+      case "*":
+        result = TheFirstNumber * TheSecondNumber;
+        break;
+      case "/":
+        if (TheSecondNumber === 0) {
+          alert("На ноль не делим");
+          break;
+        } else {
+          result = TheFirstNumber / TheSecondNumber;
+          break;
+        }
     }
-    document.getElementById("result").value = result;
-    
+    document.getElementById("result").innerHTML = result;
+  }
 }
