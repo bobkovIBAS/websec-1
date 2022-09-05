bb75ebbe0fd2679a45df0a04207bf830b6745c1e
bobkovIBAS <va.bobkov@yandex.ru>
2022-09-05 17:31:13 +0400

    error correction

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function Calculate() {
   let result;
-  let TheFirstNumber = Number(document.getElementById("TheFirstElement").value);
-  let TheSecondNumber = Number(document.getElementById("TheSecondElement").value);
+  let TheFirstNumber = document.getElementById("TheFirstElement").value;
+  let TheSecondNumber = document.getElementById("TheSecondElement").value;
   let typeOfOperation = document.getElementById("typeOfOperation").value;
 
   if (TheFirstNumber === "" || TheSecondNumber === "") {
     alert("Имеются незаполненные поля");
   } else {
+    TheFirstNumber = Number(TheFirstNumber);
+    TheSecondNumber = Number(TheSecondNumber);
     switch (typeOfOperation) {
       case "+":
         result = TheFirstNumber + TheSecondNumber;
@@ -18,7 +20,7 @@ function Calculate() {
         result = TheFirstNumber * TheSecondNumber;
         break;
       case "/":
-        if (TheSecondNumber === 0) {
+        if (Math.abs(TheSecondNumber) < Number.EPSILON) {
           alert("На ноль не делим");
           break;
         } else {
